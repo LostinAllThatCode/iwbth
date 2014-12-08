6074181a3595666c045d06649996d2dead1b3b03
lostinallthatcode <lostinallthatcode@code.com>
2014-12-08 22:51:10 +0100

    test1

--- src/main/java/org/gdesign/platformer/factories/Box2dBodyFactory.java
+++ src/main/java/org/gdesign/platformer/factories/Box2dBodyFactory.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.RayCastCallback;
+
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.World;
 
